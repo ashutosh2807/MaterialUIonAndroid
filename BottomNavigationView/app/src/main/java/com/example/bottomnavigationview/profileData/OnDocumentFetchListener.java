42156f331f981373af8108614669b9973b90c524
ashutosh2807 <56646405+ashutosh2807@users.noreply.github.com>
2023-09-10 13:10:56 +0530

    OnDocumentFetchListener

---
+++ BottomNavigationView/app/src/main/java/com/example/bottomnavigationview/profileData/OnDocumentFetchListener.java
@@ -0,0 +1,8 @@
+package com.example.bottomnavigationview.profileData;
+
+import java.util.List;
+import java.util.Map;
+
+public interface OnDocumentFetchListener {
+    void onDocumentsFetched(List<Map<String, Object>> documentList);
+}
